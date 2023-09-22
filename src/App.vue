b4631cb428a21e6f6bb6adf12b1b0c3bc94ea6e0
unknown <vlad981010@gmail.com>
2023-09-22 19:35:28 +0700

    v.0.01 try create API maps

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Geolocation_Div msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Geolocation_Div from './components/Geolocation_Div.vue'
+export default {
+  name: 'App',
+  components: {
+    Geolocation_Div
+  }
+}
+</script>
+sard
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Geolocation_Div.vue
@@ -0,0 +1,37 @@
+<template>
+<header>
+  <div id="geolocation-div">
+    <YandexMap :settings="settings" :coordinates="[32, 50]">
+    <!--Markers-->
+    </YandexMap>
+  </div>
+  <div id="text">
+    Работает или нет??????????????????????????
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: 'Geolocation_Div',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
